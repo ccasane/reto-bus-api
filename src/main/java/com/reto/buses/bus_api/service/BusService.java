6f33fec20b8c4e1f9af30815492c594d8bfec7d8
Joel Ccasane <joelccasane@gmail.com>
2025-10-03 02:22:40 -0500

    add services

---
+++ src/main/java/com/reto/buses/bus_api/service/BusService.java
@@ -0,0 +1,29 @@
+package com.reto.buses.bus_api.service;
+
+import com.reto.buses.bus_api.model.Bus;
+import com.reto.buses.bus_api.repository.BusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BusService {
+
+    @Autowired
+    private BusRepository busRepository;
+
+    public Page<Bus> getAllBuses(Pageable pageable) {
+        return busRepository.findAll(pageable);
+    }
+
+    public Optional<Bus> getBusById(Long id) {
+        return busRepository.findById(id);
+    }
+
+    public Bus saveBus(Bus bus) {
+        return busRepository.save(bus);
+    }
+}
