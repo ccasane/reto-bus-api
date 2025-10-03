adc86f351366e1ee16a83ba84ab799e121d778f3
Joel Ccasane <joelccasane@gmail.com>
2025-10-03 01:27:20 -0500

    add models

---
+++ src/main/java/com/reto/buses/bus_api/model/Marca.java
@@ -0,0 +1,23 @@
+package com.reto.buses.bus_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "marcas")
+public class Marca {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nombre;
+
+    public Marca() {}
+    public Marca(String nombre) { this.nombre = nombre; }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+}
