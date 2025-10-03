2506b13a4b516075b5a3f99798007be44e6edb56
Joel Ccasane <joelccasane@gmail.com>
2025-10-03 03:50:40 -0500

    modify repositories

--- src/main/java/com/reto/buses/bus_api/repository/MarcaRepository.java
+++ src/main/java/com/reto/buses/bus_api/repository/MarcaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
-    Optional<Marca> encontrarPorNombre(String nombre);
+    Optional<Marca> findByNombre(String nombre);
 }
